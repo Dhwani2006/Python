de47085d243aa62be3adc9d4b6a68832890941aa
Khapre Dhwani <154263286+Dhwani2006@users.noreply.github.com>
2024-12-24 19:47:17 +0530

    Delete Cls Practice directory

--- Cls Practice/Addition.py
+++
@@ -1,7 +0,0 @@
-import addmod as add
-def main():
-    sum=add.add(2,6)
-    print("The sum is:",sum)
-
-
-main()
--- Cls Practice/module.py
+++
@@ -1,8 +0,0 @@
-import math
-print(math.sqrt(16))
-print(math.pi)
-import math as M
-
-from math import *
-print(sqrt(16))
-print(pi)
